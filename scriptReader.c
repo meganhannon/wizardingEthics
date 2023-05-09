713ef8663e92a0627f54506590c3e717545d0751
meganhannon <125232535+meganhannon@users.noreply.github.com>
2023-05-09 06:39:24 -0400

    Update scriptReader.c

--- scriptReader.c
+++ scriptReader.c
@@ -7,6 +7,29 @@
 
 FILE *fp;
 
+enum {BUFSIZE = 48};
+char name[BUFSIZE];
+int choice;
+void readString(void)
+{
+   char buf[BUFSIZE];
+   int i = 0; 
+   int c;
+
+   for (;;)
+   {
+      c = fgetc(stdin);
+      if ((c == EOF) || (c == '\n')) 
+         break;
+      buf[i] = c;
+      i++;
+   }
+   buf[i] = '\0';
+
+   for (i = 0; i < BUFSIZE; i++) 
+      name[i] = buf[i];
+}
+
 
 void printDialogue(void)
 {
@@ -20,6 +43,7 @@ void printDialogue(void)
 
 void choice(void)
 {
+    int i = 0;
     char c;
    while ((c = fgetc(fp)) != '\0') 
       putc(c);
@@ -28,6 +52,23 @@ void choice(void)
    
    printf("Please select your choice: A or B?\n");
    readString();
+   
+   while (i == 0){
+   if (strcmp(name, "a") != 0 && strcmp(name, "b") != 0) {
+       if (strcmp(name, "A") != 0 && strcmp(name, "B") != 0){
+          printf("Please input a valid option.\n");
+       } else {
+      if ((strcmp(name, "A") == 0) {
+          choice = 1;
+          int i = 1;
+      }
+      if ((strcmp(name, "B") == 0) {
+          choice = 2;
+          int i = 1;
+      }
+   }
+          }
+}
 }
 
 int main(void)
