54eef2b34f52949d6b1a628af8a8f0126650c292
Megan Hannon <mh5308@princeton.edu>
2023-05-09 08:13:59 -0400

    script code working

--- scriptReader.c
+++ scriptReader.c
@@ -7,7 +7,9 @@
 enum {BUFSIZE = 48};
 char name[BUFSIZE];
 int dec;
-FILE *fp;
+FILE *script;
+int util;
+int deon;
 
 void readString(void)
 {
@@ -32,21 +34,54 @@ void readString(void)
 
 void printDialogue(void)
 {
-   char c;
-   while ((c = fgetc(fp)) != '\0') 
+   int c;
+
+   putchar('\n');
+   putchar('\n');
+   while ((c = fgetc(script)) != '&') 
       putchar(c);
 
    putchar('\n');
-   printf("Press any key to continue.\n");
+   putchar('\n');
+
    readString();
    
 }
 
+
+/* MUST be dialogue */
+void choiceMarker(void)
+{
+   int c;
+   putchar('\n');
+   /* if ! (choice a), continue through.
+      if @, (choice b) skip until next @, then
+      print as normal. */
+   if (dec == 1) {
+       while ((c = fgetc(script)) != '!')
+          c = 0;/* arbitrary action */
+       while ((c = fgetc(script)) != '!') 
+      putchar(c);
+   } else {
+       while ((c = fgetc(script)) != '@')
+          c = 0;/* arbitrary action */
+        while ((c = fgetc(script)) != '@') 
+      putchar(c);
+   }
+
+   putchar('\n');
+   putchar('\n');
+   readString();
+   
+}
+
+
 void choice(void)
 {
    int i = 0;
-   char c;
-   while ((c = fgetc(fp)) != '\0') 
+   int c;
+   putchar('\n');
+   while ((c = fgetc(script)) != '#') 
       putchar(c);
 
    putchar('\n');
@@ -58,51 +93,85 @@ void choice(void)
       if (strcmp(name, "a") != 0 && strcmp(name, "b") != 0) {
          if (strcmp(name, "A") != 0 && strcmp(name, "B") != 0){
             printf("Please input a valid option.\n");
+            readString();
          } else {
             if (strcmp(name, "A") == 0) {
                dec = 1;
-               int i = 1;
+               i = 1;
             }
             if (strcmp(name, "B") == 0) {
                dec = 2;
-               int i = 1;
+               i = 1;
             }
          }
       } else {
          if (strcmp(name, "a") == 0) {
             dec = 1;
-            int i = 1;
+            i = 1;
          }
          if (strcmp(name, "b") == 0) {
             dec = 2;
-            int i = 1;
+            i = 1;
          }
       }
    }
-
-      int main(void)
-      {
-         char *filename = "wizardScript.txt";
-         char ch;
-         fp = fopen(filename, "r");
-         dec = 0;
-         
-         if (fp == NULL)
-         {
-            printf("Error: could not open file %s", filename);
-            return 1;
-         }
-   
-       
-         while ((ch = fgetc(fp)) != EOF) {
-            if (ch = '&') {
-               printDialogue();
-            }
-            if (ch = '#') {
-               choice();
-            }
-         }
-   
-         fclose(fp);
-      }
+   putchar('\n');
+   putchar('\n');
 }
+
+   int main(void)
+   {
+      char *filename = "wizardScript.txt";
+      int ch;
+      script = fopen(filename, "r");
+      dec = 0;
+      util = 0;
+      deon = 0;
+
+      /* checks if file is valid */
+      if (script == NULL)
+      {
+         printf("Error: could not open file %s", filename);
+         return 1;
+      }
+
+       printf("Welcome to the wizarding world of wizarding ethics!\n");
+       printf("Please remember that you can press ENTER to exceed\n");
+       printf("dialogue, and you can press the 'a' or 'b' keys to\n");
+       printf("make decisions. Happy Wizarding!\n");
+       printf("\n             **PRESS ENTER TO CONTINUE**\n");
+       readString();
+
+       
+      /* goes through script */
+      while ((ch = fgetc(script)) != EOF) {
+         /* tracks regular dialogue */
+         if (ch == '&') {
+            printDialogue();
+         }
+
+         /* choice */
+         if (ch == '#') {
+            choice();
+         }
+
+         /* increments utilitarian score */
+         if (ch == '$') {
+            util++;
+         }
+
+         /* increments deontology score */
+         if (ch == '*') {
+            deon++;
+         }
+
+         /* true fork in path (cannot have choice ) */
+         if (ch == '%') {
+            choiceMarker();
+         }
+      }
+   
+      fclose(script);
+      return 0;
+   }
+
